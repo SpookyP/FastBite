ccbc584fe931e28c31291254c9822fbf42907201
Leonor <258497384+LeonorSoares26@users.noreply.github.com>
2026-07-10 22:37:42 +0100

    Domain-MenuCatalog-Entity-IMenuInterface

---
+++ FastBite.MenuCatalog/MenuCatalog.Domain/Entities/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuCatalog.Domain.Entities
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Nome {  get; set; } = string.Empty;
+        public string Descricao {  get; set; } = string.Empty;
+        public string Categoria {  get; set; } = string.Empty;
+        public string Alergenios {  get; set; } = string.Empty;
+        public decimal PrecoBase { get; set; }
+        public int LimiteDiario { get; set; }
+    }
+}
