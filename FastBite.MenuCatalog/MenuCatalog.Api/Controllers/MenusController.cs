26b94f51bd17d33dbef5b1b72e07a9924845a30f
Leonor <258497384+LeonorSoares26@users.noreply.github.com>
2026-07-14 23:18:04 +0100

    EndPoint Privato HttpGet e HttpDelete

---
+++ FastBite.MenuCatalog/MenuCatalog.Api/Controllers/MenusController.cs
@@ -0,0 +1,54 @@
+﻿using MenuCatalog.Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.Design;
+
+namespace MenuCatalog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenusController(IMenuService menuService) : ControllerBase
+    {
+        private readonly IMenuService _menuService = menuService;
+
+
+        [HttpGet("ObterPorId")]
+        public async Task<IActionResult> ObterPorId(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("ID inválido. O ID deve ser maior do que zero.");
+            }
+
+            var menuId = await _menuService.ObterPorIdAsync(id);
+
+            if (menuId == null)
+            {
+                return NotFound("Menu não encontrado.");
+            }
+
+            return Ok(menuId);
+
+        }
+
+        [HttpDelete("Eliminar")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> RemoverMenu(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("ID inválido. O ID deve ser maior do que zero.");
+            }
+
+            var menuExistente = await _menuService.ObterPorIdAsync(id);
+            if (menuExistente == null)
+            {
+                return NotFound("Menu não encontrado.");
+            }
+
+            await _menuService.RemoverMenuAsync(id);
+            return NoContent(); //204 - O servidor respondeu ao pedido, mas não retorna nenhum conteúdo.
+        }
+    }
+}
