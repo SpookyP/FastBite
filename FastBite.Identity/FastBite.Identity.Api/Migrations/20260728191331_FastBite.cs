f5b453bdf0dfa084916e6583f0e53d640815622a
Madalena <ana.ferreira.t0133592@edu.atec.pt>
2026-07-28 22:51:22 +0100

    fix da views de login/logout

---
+++ FastBite.Identity/FastBite.Identity.Api/Migrations/20260728191331_FastBite.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FastBite.Identity.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FastBite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
