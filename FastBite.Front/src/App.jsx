c2ef8d91711536b43af405f05d594f3673bb92de
Leonor <258497384+LeonorSoares26@users.noreply.github.com>
2026-09-04 17:50:54 +0100

    Configuração inicial do CartContext e CartProvider
    Criação do CartContext/Provider: Estabelece o contexto para armazenar e gerir a informação dos produtos selecionados pelo utilizador, evitando o "prop drilling" (passagem excessiva de propriedades).
    
    Integração no App.jsx: O CartProvider foi importado e envolve a raiz da aplicação, garantindo que qualquer componente descendente possa aceder ao estado do carrinho através de hooks.

--- FastBite.Front/src/App.jsx
+++ FastBite.Front/src/App.jsx
@@ -6,21 +6,24 @@ import CreateItem from './pages/Items/create-form';
 import EditItem from './pages/Items/edit-form';
 // import ShowItem from './pages/Items/details-form';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {CartProvider} from './context/CartContext';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/items" element={<ListItems />} />
-          <Route path="/items/create" element={<CreateItem />} />
-          <Route path="/items/edit" element={<EditItem />} />
-          {/* <Route path="/items/show" element={<ShowItem />} />
-          <Route path="/items/destroy" element={<DeleteItem />} /> */}
-        </Routes>
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/items" element={<ListItems />} />
+            <Route path="/items/create" element={<CreateItem />} />
+            <Route path="/items/edit" element={<EditItem />} />
+            {/* <Route path="/items/show" element={<ShowItem />} />
+            <Route path="/items/destroy" element={<DeleteItem />} /> */}
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
